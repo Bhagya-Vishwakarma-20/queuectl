9ba9b424e84d555be23f589068218ee2d533bb81
Bhagya-vishwakarma-20 <Bhagyavishwakarma88@gmail.com>
2026-07-13 06:32:43 +0530

    feat: bash execution

--- src/worker/executer.js
+++ src/worker/executer.js
@@ -1,9 +1,17 @@
 import { exec } from "child_process";
 import { promisify } from "util";
 const execAsync = promisify(exec);
+// adding bash 
+async function bash(command) {
+    return execAsync(command, {
+        shell: "C:\\Program Files\\Git\\bin\\bash.exe"
+    });
+}
+
 export async function executeJob(job){
     try{
-        await execAsync(job.command);
+        const result = await bash(job.command);
+        // console.log(result);
         // await execAsync(`start cmd /k "${job.command}"`);
         return true;
     }catch(err){
